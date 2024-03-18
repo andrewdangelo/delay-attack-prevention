01578868219fbb5452396e4af71e61937019e185
andrewdangelo <57690219+andrewdangelo@users.noreply.github.com>
2024-03-18 10:33:16 -0400

    test iptables

--- src/redirect.py
+++ src/redirect.py
@@ -11,12 +11,7 @@ def list_interfaces():
 def clean_iptables():
     os.system("sudo sysctl -w net.ipv4.ip_forward=1")
     os.system("sysctl -w net.ipv4.conf.all.send_redirects=1")
-    existing_nat = iptc.easy.dump_chain('nat','PREROUTING')
-    existing_filter = iptc.easy.dump_chain('filter','FORWARD')
-    for rule in existing_nat:
-        iptc.easy.delete_rule('nat','PREROUTING',rule)
-    for rule in existing_filter:
-        iptc.easy.delete_rule('filter','FORWARD',rule)
+    
 
 if __name__ == "__main__":
     interfaces = list_interfaces()
