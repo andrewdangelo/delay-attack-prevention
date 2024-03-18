b74c93068efd20a8ee52f6915b64d03550f967ec
andrewdangelo <57690219+andrewdangelo@users.noreply.github.com>
2024-03-18 13:48:04 -0400

    test iptables

--- src/redirect.py
+++ src/redirect.py
@@ -7,15 +7,24 @@ def list_interfaces():
     iface, index = pick(netifaces.interfaces(), 'Please choose the wireless interface used for hotspot')
     return iface
 
-def clean_nftables():
+""" def clean_nftables():
     subprocess.run([ "nft", "flush", "table", "ip", "nat"])
     subprocess.run([ "nft", "delete", "table", "ip", "nat"])
     subprocess.run([ "nft", "flush", "table", "ip", "filter"])
     subprocess.run([ "nft", "delete", "table", "ip", "filter"])
-    print("Ran nftables....")
+    print("Ran nftables....") """
+
+def list_nftables_rules():
+    result = subprocess.run(["nft", "list", "ruleset"], capture_output=True, text=True)
+    if result.returncode == 0:
+        print(result.stdout)
+    else:
+        print("Error listing nftables rules:", result.stderr)
+
+list_nftables_rules()
     
 
 if __name__ == "__main__":
     #interfaces = list_interfaces()
-    clean_nftables()
+    list_nftables_rules()
     #print(interfaces)
