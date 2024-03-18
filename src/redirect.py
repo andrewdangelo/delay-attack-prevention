0191e05a6f85ffa05f32da8f00517b9810952d8e
andrewdangelo <57690219+andrewdangelo@users.noreply.github.com>
2024-03-18 10:42:40 -0400

    test iptables

--- src/redirect.py
+++ src/redirect.py
@@ -12,9 +12,10 @@ def clean_nftables():
     subprocess.run(["sudo", "nft", "delete", "table", "ip", "nat"])
     subprocess.run(["sudo", "nft", "flush", "table", "ip", "filter"])
     subprocess.run(["sudo", "nft", "delete", "table", "ip", "filter"])
+    print("Ran nftables....")
     
 
 if __name__ == "__main__":
     interfaces = list_interfaces()
-    clean_iptables
+    clean_nftables()
     print(interfaces)
